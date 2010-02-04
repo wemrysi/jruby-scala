c7d80c7b8f39a1029d89f902b0db1379ee1f0394
Emrys Ingersoll <ingersoll@gmail.com>
2010-02-04 10:46:56 -0500

    Add test for the 'scala' namespace/method

--- lib/jruby_scala.rb
+++ lib/jruby_scala.rb
@@ -1,6 +1,10 @@
 require 'java'
 
 # Toplevel namespace for the core Scala classes.
+#
+# @example Access scala elements like you would in Java
+#   scala.collection.immutable.HashMap
+#
 def scala
   Java::Scala
 end
--- spec/jruby_scala_spec.rb
+++ spec/jruby_scala_spec.rb
@@ -1,4 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "JrubyScala" do
+  before(:all) do
+    @hmap = Java::ScalaCollectionImmutable::HashMap
+  end
+
+  it "should provide a top-level java-like namespace for scala elements" do
+    scala.collection.immutable.HashMap.should == @hmap
+  end
 end
